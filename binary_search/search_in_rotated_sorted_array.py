68ecfb040128334d03b73123cdf3637243c6df64
jfar41 <68749301+jfar41@users.noreply.github.com>
2024-05-20 07:00:55 -0700

    Search in rotated sorted array

---
+++ binary_search/search_in_rotated_sorted_array.py
@@ -0,0 +1,37 @@
+# Content:
+# originally, nums sorted in ascending order w distinct vals
+# nums possibly rotated @ unknown pivot indx
+# Given nums post-pivot
+
+# Constraints:
+# O(log n) runtime complexity
+
+# Returning:
+# return index of target if in nums or -1 if not in nums
+
+# Scratch:
+# 
+
+def search(nums, target):
+    lft = 0
+    rgt = len(nums)
+    while lft <= rgt:
+        mid = (rgt + lft) // 2
+        print(f"mid: {mid}")
+        if nums[mid] == target:
+            return mid
+        if nums[lft] <= nums[mid]:
+            if target > nums[mid] or target < nums[lft]:
+                lft = mid + 1
+            else:
+                rgt = mid - 1
+        else:
+            if target < nums[mid] or target > nums[rgt]:
+                rgt = mid - 1
+            else:
+                lft = mid + 1
+    return -1
+        
+
+if __name__ == "__main__":
+    print(search([4, 5, 6, 7, 0, 1, 2], 1))
